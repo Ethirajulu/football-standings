34e5e0df1a016ac0a9998117789eeadea8cdb710
Ethirajulu Sukumar <ethirajulu.sukumar@freshworks.com>
2025-05-19 03:51:47 +0530

    test case fixes

--- apps/backend-e2e/src/support/test-setup.ts
+++ apps/backend-e2e/src/support/test-setup.ts
@@ -1,4 +1,10 @@
+import axios from 'axios';
 import { server } from './mocks/server';
+
+// Configure Axios base URL
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.BACKEND_PORT ?? '3000';
+axios.defaults.baseURL = `http://${host}:${port}`;
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
 
